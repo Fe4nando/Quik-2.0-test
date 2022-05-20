95f36128f559adecaf2c09e67606476d0ef8a768
fernando gabriel <moreragabriel2006@gmail.com>
2022-05-20 14:52:05 +0530

    v3.11

--- assests/config.py
+++ assests/config.py
@@ -5,4 +5,4 @@
 #prefix:!
 #Color: Yellow  RGB1:(255, 255, 40)
 #color2: green  RGB2:(13, 255, 128)
-token="OTYxNDk4NjE0MTMwMTc2MDYx.GaKBBS.JmHwSjYq1nefkLmKWVam155oNAmUBt6b35dOSY"
+token="OTYxNDk4NjE0MTMwMTc2MDYx.GekHZF.nEYGiP2wWD8NKyaq6zCqpCANl8G-W6Wm75vZnI"
